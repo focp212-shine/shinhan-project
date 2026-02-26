e677c806886f2b15d834af100db94459a191dfa3
Shine Kim <focp212@gmail.com>
2026-02-27 08:09:18 +0900

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  distDir: 'build',
+};
+
+export default nextConfig;
